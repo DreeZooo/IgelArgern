5892df11656858506fb0a3af8566965f28da2dd3
Benjamin <benjamin.wojtecki@etudiant.univ-rennes.fr>
2024-10-12 00:30:03 +0200

    ajout des premières méthodes et première structuration du projet

---
+++ plateau.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "plateau.h"
+
+struct casePlateau{
+    char pile[TAILLE_MAX_PILE_HERISSON];
+    int sommet;
+};
+
+struct board{
+    casePlateau tableau[TAILLE_TABLEAU_LIGNE][TAILLE_TABLEAU_COLONNE];
+};
+
+
+//Initialiser le plateau ????
+
+void board_push(board_t* b, int line, int row, char ctn){
+    casePlateau* cellule = &b -> tableau[line][row];
+    
+    if (cellule -> sommet < TAILLE_MAX_PILE_HERISSON){
+    cellule ->pile[cellule->sommet] = ctn;
+    cellule -> sommet++;
+    }else{
+        printf("Erreur dépassement de la pile");
+        exit(1);
+    }
+}
+
+char board_pop(board_t* b, int line, int row){
+    casePlateau* cellule = &b -> tableau[line][row];
+    char sortant;
+    if(cellule -> sommet > 0){
+        sortant = cellule -> pile[cellule -> sommet-1];
+    }else{
+        printf("Erreur : Impossible d'enlever un herisson car aucun dans la pile");
+        exit(2);
+    }
+    return sortant;
+}
+int board_height(board_t* b, int line, int row){
+    casePlateau* cellule = &b -> tableau[line][row];
+    int hauteur = 0;
+    if(cellule->sommet >= 0){
+        hauteur = cellule -> sommet;
+    }else{
+        printf("La pile n'a pas de taille");
+        exit(3);
+    }
+    return hauteur;
+}
---
+++ plateau.h
@@ -0,0 +1,19 @@
+#ifndef PLATEAU_H
+#define PLATEAU_H
+
+#define TAILLE_MAX_PILE_HERISSON 5 
+#define TAILLE_TABLEAU_LIGNE 6
+#define TAILLE_TABLEAU_COLONNE 9
+
+typedef struct casePlateau casePlateau;
+typedef struct board board_t;
+
+void board_push(board_t* b, int line, int row, char ctn);
+char board_pop(board_t* b, int line, int row);
+int board_height(board_t* b, int line, int row);
+char board_top(board_t* b, int line, int row);
+char board_peek(board_t* b, int line, int row, int pos);
+void cell_print(board_t* b, int line, int row, int slice);
+void board_print(board_t* b, int highlighted_line);
+
+#endif /* plateau.h */

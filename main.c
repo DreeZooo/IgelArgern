094d4d5e556237575c0aea385a75bfc233145697
Benjamin <benjamin.wojtecki@etudiant.univ-rennes.fr>
2024-10-14 17:33:17 +0200

    Modification des tests qui sont dans un fichier un part

--- main.c
+++ main.c
@@ -9,9 +9,5 @@
 // Faire le makeFile
 
 int main(){
-    struct casePlateau c = {.pile = {'aaa'}, .sommet = 2};
-    board_t b = {c};
-    casePlateau* d =  aux(&b, 0, 0);
-    printf("%p\n", d);
-
+    printf("Hello");
 }

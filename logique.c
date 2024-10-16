a0e7580013726dc99865b25a860209df8206d7e9
NowarKa <kazem.nowar49@gmail.com>
2024-10-16 11:14:06 +0200

    Ajout de la fonction vertical_move

--- logique.c
+++ logique.c
@@ -6,14 +6,33 @@
 
 int lance_de(){
     return rand() % 6 +1;
-}PLATEAU_H
+}
 
-void move_player(){}
+void vertical_move(board_t* board, char team){
+    int line;
+    char row, top;
+    top = '0';
+    while ( top != team){
+        printf("Quel herisson voulez-vous déplacer verticalement : ");
+        scanf("%d %c", &line, &row);
+        printf("%d %d\n", line-1, (int)(row - 'a'));
+        top = board_top(board, line-1, (int)(row - 'a'));
+        if (top == '0'){
+            printf("La case sélectionnée est vide\n");
+        }
+        else if (top != team) printf("Le herisson sélectionné n'est pas le votre\n");
+
+    }
+    
+}
+
+void move_player(board_t* board, int line, char team){}
 
 void playgame(){
     srand(time(NULL));
     board_t* board = create_board();
     int random_tirage = lance_de();
     initgame(board, random_tirage);
-    printf("Le dé est tombé sur  : %d", random_tirage);
+    printf("Le dé est tombé sur  : %d\n", random_tirage);
+    vertical_move(board, 'A');
 }
